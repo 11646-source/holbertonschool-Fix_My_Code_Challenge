7dff65633404fb09d829d77decb0420cc60b79c8
Ryan364412 <bundhoor50@gmail.com>
2026-06-02 09:36:49 +0400

    F.buzz

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,9 +1,33 @@
-for i in range(1, n + 1):
-    if (i % 3) == 0 and (i % 5) == 0:
-        tmp_result.append("FizzBuzz")
-    elif (i % 3) == 0:
-        tmp_result.append("Fizz")
-    elif (i % 5) == 0:
-        tmp_result.append("Buzz")
-    else:
-        tmp_result.append(str(i))
+#!/usr/bin/python3
+""" FizzBuzz """
+
+import sys
+
+
+def fizzbuzz(n):
+    if n < 1:
+        return
+
+    result = []
+
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
+
+    print(" ".join(result))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    fizzbuzz(int(sys.argv[1]))
